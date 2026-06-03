2cacbc940423741bbd18369ef6bf4dba8373ba0f
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-06-02 22:17:57 -0300

    DRPT-03:Set up PostgreSQL database connection and health check endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.database.criar_database import engine
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"teste": "ok"}
+
+
+@app.get("/health/db")
+def health_db():
+    try:
+        with engine.connect() as con:
+            con.execute("SELECTED 1")
+
+    except Exception as e:
+        return {
+            "status": "Banco de dados disconectado",
+            "Erro": str(e)
+        }
